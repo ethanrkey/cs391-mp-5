8c5b6a8c9760d4c98cedc443fd0f68c41e09ee54
Ethan Key <ethanrkey23@gmail.com>
2025-04-18 18:26:41 -0400

    vercel sucks

--- app/r/[alias]/page.tsx
+++ app/r/[alias]/page.tsx
@@ -4,15 +4,15 @@ import { redirect } from 'next/navigation';
 const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db('url-shortener');
 
-export default async function RedirectPage({ params }: { params: Record<string, string> }) {
-    const alias = params.alias;
-  
-    const result = await db.collection('urls').findOne({ alias });
-  
-    if (result) {
-      redirect(result.url);
-    }
-  
-    return <p>Alias not found.</p>;
+// Pretend `params` is a generic object, cast manually
+export default async function RedirectPage(props: { params: { [key: string]: unknown } }) {
+  const alias = props.params.alias as string;
+
+  const result = await db.collection('urls').findOne({ alias });
+
+  if (result) {
+    redirect(result.url);
   }
-  
+
+  return <p>Alias not found.</p>;
+}
